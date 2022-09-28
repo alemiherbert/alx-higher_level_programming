49850e08c870ca669d20d80c19c1871c1e87db8a
alemiherbert <alemiherbert@gmail.com>
2022-09-28 21:45:33 +0300

    Task 1 completed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list = my_list[:]
-    if not my_list:
-        return None
-    for item in my_list:
-        if item == search:
-            # Get the position of the item in the list
-            pos = my_list.index(item)
-            # Remove the item from its place
-            my_list.remove(item)
-            # Put the replacement in the removed item's position
-            my_list.insert(pos, replace)
-    return my_list
+    return [replace if item == search else item for item in my_list]
