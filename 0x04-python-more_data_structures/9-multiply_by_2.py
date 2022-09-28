80f0ea129edea723f3dea6409862cb57c73a5902
alemiherbert <alemiherbert@gmail.com>
2022-09-29 01:00:08 +0300

    Task 9 completed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    a_dict = a.dictionary.copy()
+    a_dict = a_dictionary.copy()
 
     for key, value in a_dict.items():
         a_dict[key] = value * 2
