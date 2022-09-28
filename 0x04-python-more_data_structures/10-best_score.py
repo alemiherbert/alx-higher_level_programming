2a68288b56c6908a39d6c034eded8406da421754
alemiherbert <alemiherbert@gmail.com>
2022-09-29 01:53:44 +0300

    Task 1 completed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,5 +5,5 @@ def best_score(a_dictionary):
 
     hi_score = max(a_dictionary.values(), default=None)
     for key, value in a_dictionary.items():
-        if key == hi_score:
+        if value == hi_score:
             return key
