2927b67b1df049e47d67aa90a50d3683c58cbce6
alemiherbert <alemiherbert@gmail.com>
2022-09-28 21:32:11 +0300

    Task 1 completed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    my_list = my_list[:]
     if not my_list:
         return None
     for item in my_list:
@@ -11,3 +12,10 @@ def search_replace(my_list, search, replace):
             # Put the replacement in the removed item's position
             my_list.insert(pos, replace)
     return my_list
+
+# Tests
+_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(_list, 2, 89)
+
+print(new_list)
+print(_list)
