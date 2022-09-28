dc415ee1c4f71445e78fe5484e44fc18239e4c26
alemiherbert <alemiherbert@gmail.com>
2022-09-29 00:23:37 +0300

    Task 6 completed

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary.items())
-    
     # sorted_dict is a list of tuples.
     for key, value in sorted_dict:
         print("{0}: {1}".format(key, value))
