b264bf1900326b6f4067a4cf4794dfbb859cd6e5
alemiherbert <alemiherbert@gmail.com>
2022-10-12 12:23:06 +0300

    Task 0 complete

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    i = 0
+    p = 0
     try:
-        for item in my_list:
-            if i < x:
-                print('{}'.format(my_list[i]), end='')
-                printed += 1
+        for i in my_list:
+            if p < x:
+                print('{}'.format(my_list[p]), end='')
+                p += 1
         print()
     except TypeError:
         pass
     finally:
-        return i
+        return p
