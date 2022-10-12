e7f7239e97c58c064b34c48a48f023ab1bde7938
alemiherbert <alemiherbert@gmail.com>
2022-10-12 12:29:42 +0300

    Task 1 complete

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        value / 2
+        print("{}".format(value))
+        return True
+    except TypeError:
+        return False
