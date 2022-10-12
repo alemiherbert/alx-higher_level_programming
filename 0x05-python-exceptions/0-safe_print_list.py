5fad66a407f6568570e96f32312da1f977d89695
alemiherbert <alemiherbert@gmail.com>
2022-10-12 12:10:03 +0300

    Task 1 done

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    try:
+        for item in range(x):
+            print('{}'.format(my_list[item]), end='')
+            printed += 1
+        print()
+    except TypeError:
+        pass
+    finally:
+        return printed
+
+
