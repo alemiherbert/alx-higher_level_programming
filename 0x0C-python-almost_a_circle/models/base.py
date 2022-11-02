81f9bf2c228e11ae1a04a5e2f146e289363b5c60
alemiherbert <alemiherbert@gmail.com>
2022-11-02 22:38:54 +0300

    Style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
+"""
+
+The model with the base class for managing ids of
+all classes that inherit it.
+"""
+
 
 class Base:
+    """
+    the base class
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is not None:
-            self.__nb_objects += 1
+        """
+        nothing special
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
