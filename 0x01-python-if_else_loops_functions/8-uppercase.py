6903b329206affd34a794187938495c9c1835827
Alemi Herbert <alemiherbert@gmail.com>
2023-08-08 07:53:24 -0700

    File rename

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
         c = ord(char)
         if c >= 97 and c <= 122:
             c -= 32
-        print("{}".format(c), end='')
+        print("{}".format(chr(c)), end='')
         print("{}".format(""))
 
