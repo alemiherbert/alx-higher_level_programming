ff2e142c4aca197ff997ea90ce2266f9d65d810a
Alemi Herbert <alemiherbert@gmail.com>
2023-08-10 21:48:50 +0300

    Second attempt at task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 import hidden_4
+
 names = dir(hidden_4)
 for name in names:
-    if name.startswith("__"):
+    if not name.startswith("__"):
         print("{}".format(name))
