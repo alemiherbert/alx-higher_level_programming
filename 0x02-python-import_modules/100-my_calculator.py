e0cbbda239833ee24aa8ec776c6a034840cc43ac
Alemi Herbert <alemiherbert@gmail.com>
2023-08-10 23:08:18 +0300

    task 100 complete

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
 
-def calculate(a, op, b):
-    if (op == '+'):
-        return a + b
-    if (op == '-'):
-        return a - b
-    if (op == '*'):
-        return a * b
-    if (op == '/'):
-        return a / b
-    return None
-
-
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     from sys import argv, exit
+
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = int(argv[1])
-    operator = argv[2]
+    op = argv[2]
     b = int(argv[3])
-    result = calculate(a, operator, b)
-    if result is None:
+
+    ops = {'+': add, '-': sub, '*': mul, '/': div}
+    if op not in ops.keys():
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    print("{} {} {} = {}".format(a, operator, b, result))
+    print("{} {} {} = {}".format(a, op, b, ops[op](a, b)))
