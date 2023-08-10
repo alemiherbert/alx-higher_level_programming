10384885991c8b8fc62933f5f4148e0dbf4aa861
Alemi Herbert <alemiherbert@gmail.com>
2023-08-10 22:55:54 +0300

    Extra task 1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def calculate(a, op, b):
+    if (op == '+'):
+        return a + b
+    if (op == '-'):
+        return a - b
+    if (op == '*'):
+        return a * b
+    if (op == '/' and b != 0):
+        return a / b
+    return None
+
+if __name__ == "__main__":
+    from sys import argv, exit
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    result = calculate(a, operator, b)
+    if result == None:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
+
