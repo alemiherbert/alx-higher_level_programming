87b5e6420b65816e30928f0c2d58d7986281db18
Alemi Herbert <alemiherbert@gmail.com>
2023-08-12 23:47:01 +0300

    Code refactor

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    temp_a = temp_b = ()
-    if len(tuple_a) == 0:
-        temp_a = (0, 0)
-    if len(tuple_b) == 0:
-        temp_b = (0, 0)
-
-    if len(tuple_a) == 1:
-        temp_a = (tuple_a[0], 0)
-    if len(tuple_b) == 1:
-        temp_b = (tuple_b[0], 0)
-
-    if len(tuple_a) >= 2:
-        temp_a = (tuple_a[0], tuple_a[1])
-    if len(tuple_b) >= 2:
-        temp_b = (tuple_b[0], tuple_b[1])
+    temp_a = tuple_a + (0, 0)
+    temp_b = tuple_b + (0, 0)
     return (temp_a[0] + temp_b[0], temp_a[1] + temp_b[1])
