16751df6d80dbc74a9967e000aeb0f7dbf2524b3
Alemi Herbert <alemiherbert@gmail.com>
2023-08-17 20:28:58 +0300

    It's actually square

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,4 +2,4 @@
 
 def square_matric_map(matrix):
     new_matrix = matrix[:]
-    return list(map(lambda x: list(map(lambda y: y * 2, x)), new_matrix))
+    return list(map(lambda x: list(map(lambda y: y * y, x)), new_matrix))
