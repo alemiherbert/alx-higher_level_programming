a744eeae59620373db82a5ae809c3c8e26eabde4
Alemi Herbert <alemiherbert@gmail.com>
2023-09-05 22:08:03 -0400

    Task 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module contains a function that adds 2 integers
+"""
+
+def add_integer(a, b=98):
+    """
+    This function adds 2 integers
+    Floats are casted into ints before the arithmetic
+
+    Raises:
+        TypeError: if either a or b is not an integer or float
+    """
+    if not type(a) is int or not type(a) is float:
+        raise TypeError("a must be an integer")
+    if not type(b) is int or not type(b) is float:
+        raise TypeError("b must be an integer")
+    return (a + b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,2 @@
+# 0x07 - Test driven development
+In this chapter of the ALX experience, I learn all about tests, writing them and how they are important
