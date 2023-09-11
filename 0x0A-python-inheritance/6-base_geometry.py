75fe18e442cb860999af53420633124fd39ba791
Alemi Herbert <alemiherbert@gmail.com>
2023-09-12 01:27:22 +0300

    Add newline at the end of file

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,7 +5,7 @@
 
 class BaseGeometry:
     """ Class that does alot of nothing"""
-    
+
     def area(self):
         """Calculate the area"""
         raise Exception("area() is not implemented")
