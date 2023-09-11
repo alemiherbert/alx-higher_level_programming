0742edc5e537ede0ad044ee08b614878a1d784a6
Alemi Herbert <alemiherbert@gmail.com>
2023-09-12 00:10:43 +0300

    Task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module contains a class that inherits from the builtin list object"""
+
+def MyList(list):
+    """Implements sorted printing for the builtin list object"""
+
+    def print_sorted(self):
+        """Print a list in sorted ascending order"""
+        print(sorted(self))
