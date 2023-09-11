552ffc97bfe74518af5d6f1d84df5f1c0a0275c3
Alemi Herbert <alemiherbert@gmail.com>
2023-09-12 01:25:07 +0300

    Task 6 complete

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Lets do some numbers!!!"""
+
+
+class BaseGeometry:
+    """ Class that does alot of nothing"""
+    
+    def area(self):
+        """Calculate the area"""
+        raise Exception("area() is not implemented")
