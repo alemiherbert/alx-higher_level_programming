f9609302cf01416c7fdaf62274ac06715273c9c8
Alemi Herbert <alemiherbert@gmail.com>
2023-09-12 11:00:31 +0300

    Create rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Lets do some numbers!!!"""
+
+
+class BaseGeometry:
+    """ Class that does alot of nothing"""
+
+    def area(self):
+        """Calculate the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value
+        Args:
+            name (string): the name
+            value (int): value
+
+        Raises:
+            TypeError: if value is not an integer
+            Value Error if value is less than or equal to 0
+        """
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle object"""
+
+    def __init__(self, width, height):
+        """Initialise rectangle"""
+
+        self.integer_validator(width)
+        self.integer_validator(height)
+        self.__width = width
+        self.__height = height
