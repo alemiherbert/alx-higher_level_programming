767dc56f92324275b6255ddeb7cc2f8c6f14fabb
Alemi Herbert <alemiherbert@gmail.com>
2023-09-13 01:11:41 +0300

    Add conformity to pycodestyle

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Summary: This module contains a function that demonstrates appending textto files in python
+Summary: This module contains a function that demonstrates appending
+text to files in python
 Author: Alemi Herbert Asiki <alemiherbert@gmail.com>
 Created: September 2023
 (c) Alemi Herbert 2023
