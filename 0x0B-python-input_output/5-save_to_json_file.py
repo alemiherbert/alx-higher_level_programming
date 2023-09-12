739f390ffa335c092e3b0489ac585a256659043a
Alemi Herbert <alemiherbert@gmail.com>
2023-09-13 01:59:11 +0300

    Bugfix

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -21,4 +21,4 @@ def save_to_json_file(my_obj, filename):
         filename (string): the filename
     """
     with open(filename, "w", encoding="utf-8") as file:
-        json.dumps(my_obj, file)
+        json.dump(my_obj, file)
