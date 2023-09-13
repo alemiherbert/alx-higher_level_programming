f92c6e780a2c8ad8b46bcd65bd007ed0452cf66c
Alemi Herbert <alemiherbert@gmail.com>
2023-09-13 15:55:41 +0300

    Bug fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,6 +16,6 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if type(attrs) == list and all(type(x) == str for i in attrs):
+        if type(attrs) == list and all(type(i) == str for i in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return (self.__dict__)
