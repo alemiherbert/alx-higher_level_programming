4337f39ae57764b65d9e6a3ce7b73d1fbe7000b4
Alemi Herbert <alemiherbert@gmail.com>
2023-09-19 02:52:59 +0300

    Task 12 complete

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,11 +23,12 @@ class Square(Rectangle):
         """Return a string representation of the square"""
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                   self.width))
+
     @property
     def size(self):
         """Getter for size"""
         return (self.width)
-    
+
     @size.setter
     def size(self, value):
         """Setter for size"""
