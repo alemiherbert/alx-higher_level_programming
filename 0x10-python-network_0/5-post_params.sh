10f1071f9bfbb207f6d06f84e257547a8631b050
Alemi Herbert <alemiherbert@gmail.com>
2024-01-29 19:54:17 +0300

    Task 5 complete

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Take in a URL as an argument, send a GET request to the URL, and display the body of the response
+curl -sX POST -D "email=test@gmail.com&subject=I will always be here for PLD" "$1"
