398f96197a1878d958bfba343f0dd2f8d346e4bd
Alemi Herbert <alemiherbert@gmail.com>
2024-01-29 20:49:32 +0300

    Task 0 complete

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" fetch https://alx-intranet.hbtn.io/status"""
+
+from urllib.request import urlopen
+
+with urlopen("https://alx-intranet.hbtn.io/status") as response:
+    content = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
