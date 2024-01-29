9b704a430f21afd3693dd31789eb131aa7c5fb1f
Alemi Herbert <alemiherbert@gmail.com>
2024-01-29 20:20:08 +0300

    Bugfix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a JSON POST request to a given URL with a given JSON File
-curl -sH "Content-Type: application/json" -d "(cat "$2")" "$1"
+curl -sH "Content-Type: application/json" -d "$(cat "$2")" "$1"
