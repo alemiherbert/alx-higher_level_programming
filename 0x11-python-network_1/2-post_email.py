404f2db36406c6bdbcca45d9630406e55c0cc36b
Alemi Herbert <alemiherbert@gmail.com>
2024-01-29 21:54:56 +0300

    First remote attempt

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+
+- The email must be sent in the email variable
+- You must use the packages urllib and sys
+- You are not allowed to import packages other than urllib and sys
+- You don't need to check arguments passed to the script (number or type)
+- You must use the `with` statement
+
+"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+
+def send_post_request(url, email):
+    """Encode the email parameter"""
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+
+    with urllib.request.urlopen(url, data=data) as response:
+        content = response.read().decode("utf-8")
+        print("Response Body:")
+        print(content)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    send_post_request(url, email)
