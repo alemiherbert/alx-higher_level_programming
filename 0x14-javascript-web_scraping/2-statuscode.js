106677e7eb17f97c73d9b663f3e13a6aa3de549e
Alemi Herbert <alemiherbert@gmail.com>
2024-06-04 15:34:48 +0300

    Task 2 complete

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// This program displays the status code of a get request
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
